4bc2e09299fd621c10a703d713e698a5cf00146c
prat210 <pratyushsinha24201@gmail.com>
2024-10-09 17:27:41 +0530

    Explore post,get,delete,use

--- src/app.js
+++ src/app.js
@@ -2,15 +2,33 @@ const express = require('express');
 
 const app = express();// creating express instance or server to listen incoming http requests
 
+// this app.get will only handle get call to /users meaning it will return some data from dB
 
-app.use("/git",(req,res)=>{
-    res.send("hello ind");
+app.use("/users",(req,res)=>{
+    res.send("Hahahah I Dominate because im first and i handle all Http methods");
+})
+
+app.get("/users", (req, res) => {
+    res.send({firstName: "Pratyush", lastName: "Sinha"});
 });
 
 
+app.post("/users", (req, res) =>{
+    res.send("Data Saved Successfully in Db");
+});
+
+app.delete("/users", (req, res) =>{
+    res.send("user deleted successfully");
+});
+
+
+// this app.use will handle all Http methods to /test
 app.use("/test",(req,res)=>{
     res.send("hello from munshi");
 });
+
+
+
 
 app.listen(3000,()=>{
     console.log('listening on port 3000');
