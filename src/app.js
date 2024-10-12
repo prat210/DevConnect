e8277f9a6a2bb903b0a85eb1b55c5ef150649ee5
prat210 <pratyushsinha24201@gmail.com>
2024-10-12 16:03:21 +0530

    Mutiple route handlers

--- src/app.js
+++ src/app.js
@@ -4,28 +4,39 @@ const app = express();// creating express instance or server to listen incoming 
 
 // this app.get will only handle get call to /users meaning it will return some data from dB
 
-app.use("/users",(req,res)=>{
-    res.send("Hahahah I Dominate because im first and i handle all Http methods");
-})
-
-app.get("/users", (req, res) => {
-    res.send({firstName: "Pratyush", lastName: "Sinha"});
-});
+// app.use("/users",(req,res)=>{
+//     res.send("Hahahah I Dominate because im first and i handle all Http methods");
+// })
 
 
-app.post("/users", (req, res) =>{
-    res.send("Data Saved Successfully in Db");
-});
 
-app.delete("/users", (req, res) =>{
-    res.send("user deleted successfully");
-});
+app.use("/users", [(req, res,next)=>{
+    console.log("Handling the route user1!!");
+    next();
+    // res.send("Response");
+    
+},
+(req,res,next)=>{
+    console.log("Handling the route user 2!!");
+    // res.send("2nd response");
+    next();
+}],
+(req,res,next)=>{
+    console.log("Handling the route user 3!!");
+    // res.send("2nd response");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route user 4!!");
+    // res.send("2nd response");
+    res.send("4th Response")
+}
+
+);
+
 
 
 // this app.use will handle all Http methods to /test
-app.use("/test",(req,res)=>{
-    res.send("hello from munshi");
-});
 
 
 
