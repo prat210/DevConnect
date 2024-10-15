a58c2b81ac5d790ec0a31b6fd2466b0d35851707
prat210 <pratyushsinha24201@gmail.com>
2024-10-15 17:50:56 +0530

    doing crud operations

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const User = require("./models/user");
 app.use(express.json());
 
 
+// add a user to db
 app.post("/signup", async (req, res)=>{
 
     const user = new User(req.body);
@@ -23,6 +24,71 @@ catch(err){
 }
 });
 
+//get a single user from db
+app.get("/user", async (req, res)=>{
+    
+    const userEmail = req.body.emailId;
+
+    try{
+
+        const user = await User.findOne({ emailId: userEmail });
+        if (!user){
+            res.status(404).send("User Not Found");
+        }
+        res.send(user);
+
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+}
+)
+
+//get all users from db
+app.get("/feed", async (req,res)=>{
+    try{
+
+        const user = await User.find({});
+        if (user.length===0){
+            res.status(404).send("User Not Found");
+        }
+        res.send(user);
+
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+})
+
+//delete a user from db
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.userId;
+
+    try{
+     
+        const user = await User.findByIdAndDelete(userId);
+
+        res.send("User Deleted")
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+
+    }
+})
+
+app.patch("/user", async(req,res)=>{
+    try {
+        const userEmail = req.body.emailId;
+        const data = req.body
+        await User.findOneAndUpdate({emailId:userEmail},data);
+        // console.log(user);
+        res.send("Data Updated")
+    }
+catch(err){
+       res.status(400).send("Something went wrong");
+}
+
+})
 
 connectDb()
 .then(()=>{
